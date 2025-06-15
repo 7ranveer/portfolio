b9d7ff27beb4efd730376ceeb9f86d3b993964e2
Ranveer Gohil <ranveergohil@gmail.com>
2025-06-15 10:59:55 +0000

    Change the page title to "Ranvirsinh"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: 'My Digital Story',
+  title: 'Ranvirsinh',
   description: 'A personal portfolio website.',
 };
 
